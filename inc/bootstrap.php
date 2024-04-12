51f1140d20a4e5bf2a93ce1128a8e4bed45f323e
Andrew Nordlund <github@nordburg.ca>
2024-04-12 14:41:30 -0400

    Messing with dir structure for dev.

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -8,11 +8,11 @@ define("PROJECT_ROOT_PATH", __DIR__ . "/../");
 
 // include the base controller file
 
-require_once PROJECT_ROOT_PATH . "/Controller/Api/BaseController.php";
+require_once PROJECT_ROOT_PATH . "Controller/Api/BaseController.php";
 
 // include the use model file
 
-require_once PROJECT_ROOT_PATH . "/Model/model.php";
+require_once PROJECT_ROOT_PATH . "Model/model.php";
 
 
 ?>
