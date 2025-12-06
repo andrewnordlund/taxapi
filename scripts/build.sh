b3086ab6625d2e40cd68647aa86bd20f8958b8f0
Andrew Nordlund <github@nordburg.ca>
2025-12-06 15:39:54 -0500

    Adding logging to build.sh.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,9 @@ pwd
 
 cleanUp="true"
 
+echo "Starting out, it looks like...."
+ls -Rl
+
 if [ $# -gt 0 ]; then
   if [ $1 == "false" ]; then
     cleanUp="false"
@@ -10,21 +13,27 @@ if [ $# -gt 0 ]; then
   fi 
 fi
 
+echo "Removing..."
 if [ -d dist ]; then
   rm -Rf dist
+  echo "dist"
 fi
 if [ -d build ]; then
   rm -Rf build
+  echo "build"
 fi
 
+echo "mkdir dist build..."
 mkdir dist build
 #rsync -av --exclude="*.swp" --exclude=".DS_Store" src/ build
+echo "Moving src to build"
 if [ -e "$HOME/.gitignore" ]; then
   rsync -rlpcgoDv --exclude-from="$HOME/.gitignore" src/ build
 else
   rsync -rlpcgoDv src/ build
 #cp -Ra src/* build/
 fi
+echo "Moving build to dist"
 # delete unnecessary files			
 #cp -r build/ dist/
 # rsync -rlpcgoDv --exclude-from="$HOME/.gitignore" build/ dist
@@ -34,6 +43,7 @@ else
 #cp -a build/. dist/
   rsync -rlpcgoDv build/ dist
 fi
+echo "And now it looks like...."
 ls -Rl
 
 if [ "$cleanUp" == "true" ]; then
